2009e43ec465005bbfde06a5c791157476cf13ad
Sangeethraj K P <sangeethraj3498@gmail.com>
2026-04-12 21:51:21 +0400

    Create 03_NullHandling.js

---
+++ JavaScript/JavaScript_Basics/03_NullHandling.js
@@ -0,0 +1,12 @@
+// 1. Create a LET variable named 'industry' but do not give it a value (leave it empty)
+// 2. Write an IF statement: if industry is empty, set it to 'Educational'
+// 3. Print the industry to the console
+
+let industry;
+
+if ( !industry )
+  {
+  industry = 'Educational';
+  }
+
+console.log(industry); //output: Educational
