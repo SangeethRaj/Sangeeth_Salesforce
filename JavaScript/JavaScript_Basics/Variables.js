22036b6202015fe8bd0943e8d1fc2eca457ad5d6
Sangeethraj K P <sangeethraj3498@gmail.com>
2026-04-12 21:42:01 +0400

    Add myLead object with properties and log company

---
+++ JavaScript/JavaScript_Basics/Variables.js
@@ -0,0 +1,13 @@
+// 1. Create a constant object named 'myLead'
+// 2. Add properties: firstName, lastName, and company
+// 3. Use console.log to print the company name
+
+
+const myLead = 
+  {
+    firstName: 'Sangeeth',
+    lastName: 'Raj',
+    company: 'S Tech'
+  }
+
+console.log(myLead.company);
