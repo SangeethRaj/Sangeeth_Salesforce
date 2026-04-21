7bafbd23d16e98461f3768195d9b4fdfe624de21
Sangeethraj K P <sangeethraj3498@gmail.com>
2026-04-21 20:16:48 +0400

    Add highPriorityLeads array and lead object example

---
+++ JavaScript/JavaScript_Basics/06_ArrayMethods.js
@@ -0,0 +1,17 @@
+// 1. Create an empty array named 'highPriorityLeads'.
+// 2. Create a new lead object.
+// 3. Use the '.push()' method to add that lead to the array.
+// 4. Print the length of the array using '.length'.
+
+const highPriorityLeads = [];
+
+const myLead = 
+  {
+    firstName: 'Sangeeth',
+    lastName: 'Raj',
+    company: 'S Tech'
+  }
+
+highPriorityLeads.push(myLead);
+
+console.log(highPriorityLeads.length);
