2de16edef94da017398b3e744ea53a750e4e1452
Sangeethraj K P <sangeethraj3498@gmail.com>
2026-04-21 20:08:45 +0400

    Add leadList array with lead objects

---
+++ JavaScript/JavaScript_Basics/04_LeadArrays.js
@@ -0,0 +1,18 @@
+// 1. Create a CONSTANT array named 'leads'
+// 2. Inside the array, add two lead objects. 
+//    Each should have: firstName and company.
+// 3. Print the entire array to the console.
+
+const leadList = [
+
+  {
+    firstName: 'Sangeeth',
+    company: 'Dotts'
+  },
+  {
+    firstName: 'Sangeeth1',
+    company: 'Dotts1'
+  },
+  ]
+
+console.log(leadList);
